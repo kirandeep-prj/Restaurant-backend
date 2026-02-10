850a23b49457d467da7653307d82f47353bc011a
kirandeep <kirandeep@skycap.in>
2026-02-10 16:07:04 +0530

    update report

--- controllers/staff/report.js
+++ controllers/staff/report.js
@@ -1,6 +1,8 @@
 const Order = require("../../models/order");
 const {Parser} = require("json2csv")
 const {getTodayRange, getWeeklyRange, getMonthlyRange} = require("../../utils/dataRanges");
+const Menu = require("../../models/menu");
+const User = require("../../models/user");
 
 exports.getOrderCounts = async(req,res)=>{
     const today = getTodayRange();
@@ -33,7 +35,67 @@ exports.getOrderCounts = async(req,res)=>{
     });
 };
 
-exports.exportCSV = async(req,res)=>{
+exports.getMenuCount = async(req,res)=>{
+    const today = getTodayRange();
+    const weekly= getWeeklyRange();
+    const monthly= getMonthlyRange();
+
+    const todayCount= await Menu.countDocuments({
+        createdAt:{
+            $gte:today.start,
+            $lte:today.end
+        }
+    });
+    const weeklyCount= await Menu.countDocuments({
+        createdAt:{
+            $gte:weekly.start,
+            $lte:weekly.end
+        }
+    });
+    const monthlyCount= await Menu.countDocuments({
+        createdAt:{
+            $gte:monthly.start,
+            $lte:monthly.end
+        }
+    });
+    res.status(200).json({
+        today:todayCount,
+        weekly:weeklyCount,
+        monthly:monthlyCount
+    });
+};
+
+exports.getUserCount = async(req,res)=>{
+    const today = getTodayRange();
+    const weekly = getWeeklyRange();
+    const monthly = getMonthlyRange();
+
+    const todayCount = await User.countDocuments({
+        createdAt:{
+            $gt:today.start,
+            $lte:today.end
+        }
+    });
+    const weeklyCount = await User.countDocuments({
+        createdAt:{
+            $gte:weekly.start,
+            $lte:weekly.end
+        }
+    });
+    const monthlyCount = await User.countDocuments({
+        createdAt:{
+            $gt:monthly.start,
+            $lte:monthly.end
+        }
+    });
+    res.status(200).json({
+        today:todayCount,
+        weekly:weeklyCount,
+        monthly:monthlyCount
+    });
+};
+
+exports.exportOrderCSV = async(req,res)=>{
     const orders = await Order.find().lean();
 
     const json2csv = new Parser();
@@ -43,4 +105,26 @@ exports.exportCSV = async(req,res)=>{
     res.attachment("orders.csv");
     res.send(csv);
 };
+
+exports.exportMenuCSV = async(req,res)=>{
+    const menu = await Menu.find().lean();
+
+    const json2csv = new Parser();
+    const csv = json2csv.parse(menu);
+
+    res.header("Content-type","text/csv");
+    res.attachment("menu.csv");
+    res.send(csv);
+};
+
+exports.exportUserCSV = async(req,res)=>{
+    const user= await User.find().lean();
+
+    const json2csv = new Parser();
+    const csv = json2csv.parse(user);
+
+    res.header("Content-Type","text/csv");
+    res.attachment("user.csv");
+    res.send(csv);
+}
 
--- routes/staff/report.js
+++ routes/staff/report.js
@@ -5,6 +5,10 @@ const restrictTo = require("../../middleware/restrictTo");
 const router = express.Router();
 
 router.get("/order-counts",auth,restrictTo("staff"),report.getOrderCounts);
-router.get("/export/csv",auth,restrictTo("staff"),report.exportCSV);
+router.get("/menu-counts",auth,restrictTo("staff"),report.getMenuCount);
+router.get("/user-counts",auth,restrictTo("staff"),report.getUserCount);
+router.get("/export/order/csv",auth,restrictTo("staff"),report.exportOrderCSV);
+router.get("/export/menu/csv",auth,restrictTo("staff"),report.exportMenuCSV);
+router.get("/export/user/csv",auth,restrictTo("staff"),report.exportUserCSV);
 
 module.exports=router;
